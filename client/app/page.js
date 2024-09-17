1d59dee24adc6fd93e74d46eb1b83e82c279a6e2
Emmy Veselinov <emmyveselinov@gmail.com>
2024-09-17 01:03:56 -0400

    displayed all trips from the db via server

--- client/app/page.js
+++ client/app/page.js
@@ -3,7 +3,7 @@
 import styles from "./page.module.css";
 import React, {useEffect, useState} from 'react'
 
-export default function Home() {
+export function Home() {
   
   const [message, setMessage] = useState("Loading")
 
@@ -24,6 +24,41 @@ export default function Home() {
       <main className={styles.main}>
         <div>Return message from server</div>
         <div>{message}</div>
+      </main>
+    </div>
+  );
+}
+
+export default function GetTrips() {
+  
+  const [message, setMessage] = useState("Loading")
+  const [trips, setTripData] = useState([])
+
+  console.log(process.env.NEXT_PUBLIC_SERVER_URL + "/get-trips")
+  useEffect(() => {
+    fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/get-trips").then(
+      response => response.json()
+    ).then(
+      data => {
+        setTripData(data.data)
+      }
+    )
+  }, [])
+  
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div>Get all the trips from the db</div>
+        <div>{trips.map(trip => 
+          <div>
+            <p>Trip {trip.id}</p>
+            <br></br>
+            <li>{trip.name}</li>
+            <li>{trip.start_date} to {trip.end_date}</li>
+            <li>${trip.budget}</li>
+            <br></br>
+          </div>)}
+        </div>
       </main>
     </div>
   );
