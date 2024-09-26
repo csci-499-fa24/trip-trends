a5b70058439b5211a27cce7e08263835bb7202ac
Ellen Avrumova <e.avrumova@gmail.com>
2024-09-26 17:31:28 -0400

    Signup styling

--- client/app/signup/page.js
+++ client/app/signup/page.js
@@ -6,18 +6,24 @@ import Image from 'next/image';
 import logo from '../img/Logo.png'
 import '../css/signup.css'
 import axios from 'axios';
+import { Inria_Sans } from 'next/font/google';
 
 const googleID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+
+const inriaSans = Inria_Sans({
+    weight: ['300', '400', '700'],
+    subsets: ['latin']
+});
 
 function Signup() {
 
     const handleLoginSuccess = async (response) => {
-        const token = {token: response.credential}
+        const token = { token: response.credential }
 
         axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/auth/google`, token)
             .then(response => {
                 console.log(response)
-                window.location.href = '/homepage'; 
+                window.location.href = '/homepage';
                 // toast to tell user success
                 // navigate to home page
             })
@@ -34,19 +40,20 @@ function Signup() {
 
     return (
         <GoogleOAuthProvider clientId={googleID}>
-            <div className='container text-center'>
+            <div className={`container text-center ${inriaSans.className}`}>
                 <div className='row'>
                     <div className='col-md-6 logo'>
                         <Image src={logo} alt="Logo" width={"15vw"} height={"15vw"} />
                     </div>
                     <div className='col-md-6 signIn'>
                         <div>
-                            <h2>Sign In With Google</h2>
+                            <h1 className='signInGoogle'>Sign In With Google</h1>
                             <GoogleLogin
                                 onSuccess={handleLoginSuccess}
                                 onError={handleLoginFailure}
                             />
-                            <p>Welcome to Trip Trends! We hope you enjoy your adventure with our easily accessible and modernized app. Here’s to a great time!</p>
+                            <h2 className='welcome'>Welcome to Trip Trends!</h2>
+                            <p className='description'>We hope you enjoy your adventure with our easily accessible and modernized app. Here’s to a great time!</p>
                         </div>
                     </div>
                 </div>
