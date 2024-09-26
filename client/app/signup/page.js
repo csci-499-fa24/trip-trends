a2ff0275974098c39a3ae6ca8007456911bd05cf
Ellen Avrumova <e.avrumova@gmail.com>
2024-09-26 10:49:23 -0400

    post API call works and redirects to homepage when successful

--- client/app/signup/page.js
+++ client/app/signup/page.js
@@ -2,53 +2,40 @@
 
 import React from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
 import Image from 'next/image';
 import logo from '../img/Logo.png'
 import '../css/signup.css'
+import axios from 'axios';
 
 const googleID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
 function Signup() {
+
     const handleLoginSuccess = async (response) => {
-        // console.log('Encoded JWT ID token:', response.credential);
-        const token = response.credential
+        const token = {token: response.credential}
 
-        // console.log(token)
-
-        const userData = jwtDecode(token)
-
-        const name = userData.name;
-        const email = userData.email;
-        const imageUrl = userData.picture;
-
-        // console.log('Name:', name);
-        // console.log('Email:', email);
-        // console.log('Image URL:', imageUrl);
-
-        const splitName = name.split(" ")
-        const firstName = splitName[0]
-        const lastName = splitName[1]
-        // console.log("FirstName: ", firstName)
-        // console.log("LastName: ", lastName)
-
-        const signUpData = {
-            fname: firstName,
-            lname: lastName,
-            email: email,
-            imageUrl: imageUrl
-        }
-
-        // send to backend
+        axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/auth/google`, token)
+            .then(response => {
+                console.log(response)
+                window.location.href = '/homepage'; 
+                // toast to tell user success
+                // navigate to home page
+            })
+            .catch(error => {
+                console.log(token)
+                console.error(error)
+                // toast to user to say there's an error
+            });
     };
 
     const handleLoginFailure = (error) => {
         console.error('Signup failed:', error);
+        // toast to user to say there's an error
     };
 
     return (
         <GoogleOAuthProvider clientId={googleID}>
-            <div class='container text-center'>
+            <div className='container text-center'>
                 <div className='row'>
                     <div className='col-md-6 logo'>
                         <Image src={logo} alt="Logo" width={"15vw"} height={"15vw"} />
