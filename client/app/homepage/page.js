640741e1300eddccd2f33d4c3b62b15159b2fc0c
Alima Zaman <alimazaman@Alimas-MacBook-Air.local>
2024-09-27 20:33:57 -0400

    added logout

--- client/app/homepage/page.js
+++ client/app/homepage/page.js
@@ -1,11 +1,22 @@
 'use client';
 
 import React from 'react';
+import { GoogleOAuthProvider, googleLogout } from '@react-oauth/google';
+import '../css/homepage.css'
+
+const googleID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
 function Homepage() {
-    return(
-        <p>HOMEPAGE</p>
+    const handleLogout = () => {
+        googleLogout();
+        localStorage.removeItem("token");
+        window.location.href = '/signup';
+    };
+
+    return (
+        <GoogleOAuthProvider clientId={googleID}>
+            <button onClick={handleLogout} className='logout'>Logout</button>
+        </GoogleOAuthProvider>
     );
 }
-
 export default Homepage;
