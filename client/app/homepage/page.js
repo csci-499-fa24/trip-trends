e8ed9701186bb0fc4feff1742e9c59d3a5b614b0
Tasmina Hassan <tasmina.hassan10@myhunter.cuny.edu>
2024-09-28 18:44:34 -0400

    created basic homepage

--- client/app/homepage/page.js
+++ client/app/homepage/page.js
@@ -1,11 +1,63 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
+import logo from '../img/Logo.png';
+import '../css/homepage.css';
+import axios from 'axios';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-function Homepage() {
-    return(
-        <p>HOMEPAGE</p>
+// Import your custom pin icon
+import pinIcon from '../img/Pin.png'; // Change the path as needed
+
+// Create a custom icon
+const custompinIcon = L.icon({
+    iconUrl: pinIcon.src,
+    iconSize: [25, 30], // Size of the icon
+    iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+    popupAnchor: [1, -34], // Point from which the popup should open relative to the iconAnchor
+});
+
+function homepage() {
+    return (
+        <div className="dashboard">
+            {/* Header Section */}
+            <header className="header">
+                <div className="logo-container">
+                    <Image src={logo} alt="Logo" width={300} height={300} />
+                </div>
+                <div className="left-rectangle"></div>
+                <div className="right-rectangle"></div>
+            </header>
+
+            {/* Welcome Section */}
+            <div className="welcome-section">
+                <h1>Welcome Back, [Name]!</h1>
+                <p>See everywhere you've gone:</p>
+            </div>
+
+             {/* Map Section */}
+             <MapContainer center={[40.7128, -74.0060]} zoom={13} style={{ height: '400px', width: '100%' }}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[40.7128, -74.0060]} icon={custompinIcon}>
+                    <Popup>
+                        A marker for New York City!
+                    </Popup>
+                </Marker>
+            </MapContainer>
+
+           {/* Recent Trips Section */}
+           <div className="recent-trips">
+                <h2>Recent Trips</h2>
+                <p>No trips available for now.</p>
+            </div>
+        </div>
     );
 }
 
-export default Homepage;
+export default homepage;
