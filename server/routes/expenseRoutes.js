de8f72d498ceea74642ea052e3564a84f75fe2d5
clin-10 <lin.christin8@gmail.com>
2024-10-02 11:09:35 -0400

    add endpoint for GETting expenses by trip

--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -5,6 +5,7 @@ const ExpenseController = require('../controllers/ExpenseController');
 router.post("/create-expense", ExpenseController.createExpense);
 router.get("/get-expenses", ExpenseController.getExpenses);
 router.get("/get-expense/:id", ExpenseController.getExpenseById);
+router.get("/:tripId/get-trip-expenses/", ExpenseController.getExpensesByTripId);
 router.put("/update-expense/:id", ExpenseController.updateExpense);
 router.delete("/delete-expense/:id", ExpenseController.deleteExpense);
 
