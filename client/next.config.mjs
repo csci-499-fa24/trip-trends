afb6ceeef358f700927aca50d26fff3791797036
clin-10 <lin.christin8@gmail.com>
2024-10-10 12:34:07 -0400

    add redirect to homepage

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/homepage',
+                permanent: true,
+            },
+        ];
+    },
+};
 export default nextConfig;
