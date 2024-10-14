3475fc0657485c6824efe675dda71fbe9d614022
clin-10 <lin.christin8@gmail.com>
2024-10-13 20:09:54 -0400

    resolve merge conflicts

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        domains: ['lh3.googleusercontent.com'], // Allow images from this domain
+    },
     async redirects() {
         return [
             {
