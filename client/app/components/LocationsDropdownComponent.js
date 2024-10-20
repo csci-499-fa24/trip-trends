79c13efa9b9f0284eecb64737d6b3f14fa3d070d
Ellen Avrumova <e.avrumova@gmail.com>
2024-10-20 14:34:28 -0400

    Added trip locations dropdown to single trip view

---
+++ client/app/components/LocationsDropdownComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../css/dropdown.css'
+
+const LocationsDropdownComponent = ({ tripLocations }) => {
+    return (
+        <div className='dropdown-container'>
+            <div className='row'>
+                <div className='col'>
+                    <div className="btn-group" style={{ marginBottom: "0" }}>
+                        <button
+                            type="button"
+                            className="btn dropdown-button dropdown-toggle"
+                            data-bs-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            Trip Locations
+                        </button>
+                        <div className="dropdown-menu">
+                            {tripLocations.map((location, index) => (
+                                <div key={index} className="dropdown-item" href="#">
+                                    {location}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LocationsDropdownComponent;
