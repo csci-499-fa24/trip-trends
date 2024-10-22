340abe52a48d898a14f383ebe6aa89c628ff3c38
Ellen Avrumova <e.avrumova@gmail.com>
2024-10-21 21:23:43 -0400

    prepopulated edit trip form

--- client/app/components/singletrip/EditTripComponent.js
+++ client/app/components/singletrip/EditTripComponent.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import '../../css/singletrip.css';
+import '../css/singletrip.css';
 
 const EditTripComponent = ({ tripId, tripData, tripLocations, userRole }) => {
     // hide button if user is not trip owner
@@ -12,14 +12,14 @@ const EditTripComponent = ({ tripId, tripData, tripLocations, userRole }) => {
     const [name, setName] = useState(tripData?.name || "");
     const [startDate, setStartDate] = useState(tripData?.start_date || "");
     const [endDate, setEndDate] = useState(tripData?.end_date || "");
-    const [budget, setBudget] = useState(tripData?.budget || "");    
+    const [budget, setBudget] = useState(tripData?.budget || "");
 
-    console.log(name, startDate, endDate, budget);
-    
+    // console.log(name, startDate, endDate, budget);
+
     const handleEdit = async (e) => {
-        e.preventDefault(); 
+        e.preventDefault();
         try {
-            const requestBody = { name, startDate, endDate, budget }; 
+            const requestBody = { name, startDate, endDate, budget };
             await axios.put(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/trips/${tripId}`, requestBody);
             toast.success("Trip updated successfully!");
             setIsOpen(false);
@@ -27,7 +27,7 @@ const EditTripComponent = ({ tripId, tripData, tripLocations, userRole }) => {
             console.error('Error updating trip:', error);
             toast.error("Error updating trip. Please try again.");
         }
-        
+
     };
 
     return (
@@ -35,11 +35,11 @@ const EditTripComponent = ({ tripId, tripData, tripLocations, userRole }) => {
             <div class="icon-div" tooltip="Edit Trip" tabindex="0">
                 <div class="icon-SVG">
                     <svg
-                    onClick={() => setIsOpen(true)}
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                        onClick={() => setIsOpen(true)}
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                     </svg>
-                <span class="icon-text">Edit Trip</span>
+                    <span class="icon-text">Edit Trip</span>
                 </div>
             </div>
 
@@ -54,7 +54,7 @@ const EditTripComponent = ({ tripId, tripData, tripLocations, userRole }) => {
                                 Name:
                                 <input
                                     type="text"
-                                    value={name}
+                                    value={tripData.data.name}
                                     onChange={(e) => setName(e.target.value)}
                                     required
                                 />
@@ -64,7 +64,7 @@ const EditTripComponent = ({ tripId, tripData, tripLocations, userRole }) => {
                                 Start Date:
                                 <input
                                     type="date"
-                                    value={startDate}
+                                    value={tripData.data.start_date}
                                     onChange={(e) => setStartDate(e.target.value)}
                                     required
                                 />
@@ -74,7 +74,7 @@ const EditTripComponent = ({ tripId, tripData, tripLocations, userRole }) => {
                                 End Date:
                                 <input
                                     type="date"
-                                    value={endDate}
+                                    value={tripData.data.end_date}
                                     onChange={(e) => setEndDate(e.target.value)}
                                     required
                                 />
@@ -84,7 +84,7 @@ const EditTripComponent = ({ tripId, tripData, tripLocations, userRole }) => {
                                 Budget:
                                 <input
                                     type="number"
-                                    value={budget}
+                                    value={tripData.data.budget}
                                     onChange={(e) => setBudget(e.target.value)}
                                     required
                                 />
