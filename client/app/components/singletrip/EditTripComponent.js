d3c5277bd35e8700588667a05700c44b865d6c3b
Krina Patel <KRINA.PATEL96@myhunter.cuny.edu>
2024-10-21 21:32:03 -0400

    changed path

--- client/app/components/singletrip/EditTripComponent.js
+++ client/app/components/singletrip/EditTripComponent.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import '../css/singletrip.css';
+import '../../css/singletrip.css';
 
 const EditTripComponent = ({ tripId, tripData, tripLocations, userRole }) => {
     // hide button if user is not trip owner
