ae43c2d3050b06b26c4d55e09b2648fc147ee052
Emmy Veselinov <emmyveselinov@gmail.com>
2024-10-28 20:52:08 -0400

    updated route name and deleted unused route

--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const ExpenseController = require('../controllers/ExpenseController');
 
 router.post("/trips/:tripId/", ExpenseController.createExpense);
-router.post("/trips/:tripId/upload", ExpenseController.uploadReceipt);
 router.get("/", ExpenseController.getExpenses);
 router.get("/:expenseId", ExpenseController.getExpenseById);
 router.get("/trips/:tripId/", ExpenseController.getExpensesByTripId);
-router.get("/:expenseId/img", ExpenseController.getReceiptImageByExpenseId);
+router.get("/:expenseId/image", ExpenseController.getReceiptImageByExpenseId);
 router.put("/:expenseId", ExpenseController.updateExpense);
 router.delete("/:expenseId", ExpenseController.deleteExpense);
 
