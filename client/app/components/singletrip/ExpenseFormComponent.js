d2c9beffa4992e15e65f07ba414faa8614ba5976
Emmy Veselinov <emmyveselinov@gmail.com>
2024-11-11 16:34:04 -0500

    called the endpoint to extract data using OCR, and populated expense form

--- client/app/components/singletrip/ExpenseFormComponent.js
+++ client/app/components/singletrip/ExpenseFormComponent.js
@@ -24,8 +24,15 @@ const ExpenseFormComponent = ({
 
     const handleFormData = (formData) => {
         // set form data from receipt component
-        const file = formData.get('image')
-        setNewExpenseData((prevData) => ({ ...prevData, image: formData.get('image') }));
+        const name = formData.get('name');
+        const amount = formData.get('amount');
+        const category = formData.get('category');
+        const currency = formData.get('currency');
+        const posted = formData.get('posted'); // posted date
+        const image_file = formData.get('file')
+
+        setSelectedCurrency(currency);
+        setNewExpenseData((prevData) => ({ ...prevData, name: name, amount: amount, category: category, currency: currency, posted: posted, image: image_file }));
         // console.log("Received form data from child:", file);
     };
 
@@ -33,126 +40,128 @@ const ExpenseFormComponent = ({
         <div className="expense-form">
             {isPopUpVisible && (
                 <div className="modal">
-                    <div className="modal-content">
-                        <span className="close" onClick={() => setPopUpVisible(false)}>&times;</span>
-                        <h2 className="new-expense-title">New Expense</h2>
-                        <br></br>
-                        <div className="expense-row-container">
+                    {isPopUpVisible && (
+                        <div className="modal-content">
+                            <span className="close" onClick={() => setPopUpVisible(false)}>&times;</span>
+                            <h2 className="new-expense-title">New Expense</h2>
                             <br></br>
-                            <form onSubmit={submitNewExpense}>
-                                <label className="new-expense-field-label">
-                                    Expense Name:
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        value={newExpenseData.name}
-                                        onChange={newExpenseInputChange}
-                                        required
-                                    />
-                                </label>
-
-                                <div className="field-pair">
-                                    <label className="new-expense-field-label half-width">
-                                        Amount:
+                            <div className="expense-row-container">
+                                <br></br>
+                                <form onSubmit={submitNewExpense}>
+                                    <label className="new-expense-field-label">
+                                        Expense Name:
                                         <input
-                                            type="number"
-                                            name="amount"
-                                            value={newExpenseData.amount}
+                                            type="text"
+                                            name="name"
+                                            value={newExpenseData.name}
                                             onChange={newExpenseInputChange}
                                             required
                                         />
                                     </label>
-                                    <label className="new-expense-field-label half-width">
-                                        Currency:
-                                        <select
-                                            name="currency"
-                                            value={selectedCurrency}
-                                            onChange={(e) => {
-                                                setSelectedCurrency(e.target.value); // Update selected currency state
-                                                newExpenseInputChange(e); // Call your input change handler
-                                            }}
-                                            required
-                                        >
-                                            <option value="">Select Currency</option>
 
-                                            {/* Display the selected currency at the top if it exists and it's not USD */}
-                                            {selectedCurrency && selectedCurrency !== "USD" && (
-                                                <option value={selectedCurrency}>{selectedCurrency}</option>
-                                            )}
+                                    <div className="field-pair">
+                                        <label className="new-expense-field-label half-width">
+                                            Amount:
+                                            <input
+                                                type="number"
+                                                name="amount"
+                                                value={newExpenseData.amount}
+                                                onChange={newExpenseInputChange}
+                                                required
+                                            />
+                                        </label>
+                                        <label className="new-expense-field-label half-width">
+                                            Currency:
+                                            <select
+                                                name="currency"
+                                                value={selectedCurrency}
+                                                onChange={(e) => {
+                                                    setSelectedCurrency(e.target.value); // Update selected currency state
+                                                    newExpenseInputChange(e); // Call your input change handler
+                                                }}
+                                                required
+                                            >
+                                                <option value="">Select Currency</option>
 
-                                            {/* Recommended currencies section */}
-                                            {otherCurrencies
-                                                .filter(code => code !== selectedCurrency) // Exclude selected currency
-                                                .length > 0 && (
-                                                    <optgroup label="Recommended">
-                                                        {otherCurrencies
-                                                            .filter(code => code !== selectedCurrency) // Exclude selected currency
-                                                            .map((code, index) => (
-                                                                <option key={`other-${index}`} value={code}>{code}</option>
-                                                            ))}
-                                                    </optgroup>
+                                                {/* Display the selected currency at the top if it exists and it's not USD */}
+                                                {selectedCurrency && selectedCurrency !== "USD" && (
+                                                    <option value={selectedCurrency}>{selectedCurrency}</option>
                                                 )}
 
-                                            {/* Always place USD after other currencies */}
-                                            <optgroup label="Other">
-                                                <option value="USD">USD</option>
+                                                {/* Recommended currencies section */}
+                                                {otherCurrencies
+                                                    .filter(code => code !== selectedCurrency) // Exclude selected currency
+                                                    .length > 0 && (
+                                                        <optgroup label="Recommended">
+                                                            {otherCurrencies
+                                                                .filter(code => code !== selectedCurrency) // Exclude selected currency
+                                                                .map((code, index) => (
+                                                                    <option key={`other-${index}`} value={code}>{code}</option>
+                                                                ))}
+                                                        </optgroup>
+                                                    )}
 
-                                                {/* Display remaining currency codes, excluding selectedCurrency and other currencies */}
-                                                {currencyCodes
-                                                    .filter(code => code !== selectedCurrency && code !== "USD" && !otherCurrencies.includes(code))
-                                                    .map((code) => (
-                                                        <option key={code} value={code}>{code}</option>
-                                                    ))}
-                                            </optgroup>
-                                        </select>
-                                    </label>
+                                                {/* Always place USD after other currencies */}
+                                                <optgroup label="Other">
+                                                    <option value="USD">USD</option>
 
-                                </div>
+                                                    {/* Display remaining currency codes, excluding selectedCurrency and other currencies */}
+                                                    {currencyCodes
+                                                        .filter(code => code !== selectedCurrency && code !== "USD" && !otherCurrencies.includes(code))
+                                                        .map((code) => (
+                                                            <option key={code} value={code}>{code}</option>
+                                                        ))}
+                                                </optgroup>
+                                            </select>
+                                        </label>
 
-                                <div className="field-pair">
-                                    <label className="new-expense-field-label half-width">
-                                        Category:
-                                        <select
-                                            name="category"
-                                            value={newExpenseData.category}
-                                            onChange={newExpenseInputChange}
-                                            required
-                                        >
-                                            <option value="">Select Category</option>
-                                            {expenseCategories.map((category) => (
-                                                <option key={category} value={category}>{category}</option>
-                                            ))}
-                                        </select>
-                                    </label>
-                                    <label className="new-expense-field-label half-width">
-                                        Date:
+                                    </div>
+
+                                    <div className="field-pair">
+                                        <label className="new-expense-field-label half-width">
+                                            Category:
+                                            <select
+                                                name="category"
+                                                value={newExpenseData.category}
+                                                onChange={newExpenseInputChange}
+                                                required
+                                            >
+                                                <option value="">Select Category</option>
+                                                {expenseCategories.map((category) => (
+                                                    <option key={category} value={category}>{category}</option>
+                                                ))}
+                                            </select>
+                                        </label>
+                                        <label className="new-expense-field-label half-width">
+                                            Date:
+                                            <input
+                                                type="date"
+                                                name="posted"
+                                                value={newExpenseData.posted}
+                                                onChange={newExpenseInputChange}
+                                                required
+                                            />
+                                        </label>
+                                    </div>
+
+                                    <label className="new-expense-field-label">
+                                        Notes:
                                         <input
-                                            type="date"
-                                            name="posted"
-                                            value={newExpenseData.posted}
+                                            type="text"
+                                            name="notes"
+                                            value={newExpenseData.notes}
                                             onChange={newExpenseInputChange}
-                                            required
                                         />
                                     </label>
+                                    <button type="submit" className="submit-new-expense-button">Create</button>
+                                </form>
+                                <div className="upload-section">
+                                    <ReceiptImageComponent tripId={tripId} handleFormData={handleFormData} />
                                 </div>
 
-                                <label className="new-expense-field-label">
-                                    Notes:
-                                    <input
-                                        type="text"
-                                        name="notes"
-                                        value={newExpenseData.notes}
-                                        onChange={newExpenseInputChange}
-                                    />
-                                </label>
-                                <button type="submit" className="submit-new-expense-button">Create</button>
-                            </form>
-                            <div className="upload-section">
-                                <ReceiptImageComponent tripId={tripId} handleFormData={handleFormData} />
                             </div>
-
                         </div>
-                    </div>
+                    )}
                 </div>
             )}
         </div>
