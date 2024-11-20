ed8502ca752ee64fc659f62298d5d348030392d4
Krina Patel <KRINA.PATEL96@myhunter.cuny.edu>
2024-11-20 11:32:50 -0500

    basic discoverpage setup

---
+++ client/app/discover/page.js
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const DiscoverPage = () => {
+  return (
+    <div className="page-container">
+      <h1>Welcome to the Discover Page!</h1>
+    </div>
+  );
+};
+
+export default DiscoverPage;
