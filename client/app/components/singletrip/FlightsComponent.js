e3cdac6e02249c681ed9613e083263e76fb08082
Emmy Veselinov <emmyveselinov@gmail.com>
2024-11-26 13:12:03 -0500

    edited comment for redeployment

--- client/app/components/singletrip/FlightsComponent.js
+++ client/app/components/singletrip/FlightsComponent.js
@@ -46,6 +46,7 @@ const FlightsComponent = ({ tripId }) => {
             });
     };
 
+    // SDK for destination currencies
     const amadeus = new Amadeus({
         clientId: process.env.NEXT_PUBLIC_AMADEUS_CLIENT_ID,
         clientSecret: process.env.NEXT_PUBLIC_AMADEUS_CLIENT_SECRET,
