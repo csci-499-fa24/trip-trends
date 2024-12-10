4bf24886757770309b2a0301c6213738004a7904
Emmy Veselinov <emmyveselinov@gmail.com>
2024-12-10 14:26:03 -0500

    fixed up positioning of link icons in plaid buttons

--- client/app/components/homepage/PlaidButtonComponent.js
+++ client/app/components/homepage/PlaidButtonComponent.js
@@ -237,15 +237,17 @@ const PlaidLinkComponent = ({ onSuccess }) => {
             ) : error ? (
                 <div className="error-message">{error}</div>
             ) : accessToken ? (
-                <div>
+                <div className="plaid-link-button-wrapper">
                     <button
                         className="plaid-link-button"
                         onClick={resetPlaidConnection}
                     >
                         
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6" width="20" height="20">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.181 8.68a4.503 4.503 0 0 1 1.903 6.405m-9.768-2.782L3.56 14.06a4.5 4.5 0 0 0 6.364 6.365l3.129-3.129m5.614-5.615 1.757-1.757a4.5 4.5 0 0 0-6.364-6.365l-4.5 4.5c-.258.26-.479.541-.661.84m1.903 6.405a4.495 4.495 0 0 1-1.242-.88 4.483 4.483 0 0 1-1.062-1.683m6.587 2.345 5.907 5.907m-5.907-5.907L8.898 8.898M2.991 2.99 8.898 8.9" />
-                    </svg> <br></br>
+                        <div style={{ display: 'inline-flex', marginRight: '5px', alignItems: 'center' }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 26 25" strokeWidth="1.5" stroke="currentColor" className="size-6" width="20" height="20">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M13.181 8.68a4.503 4.503 0 0 1 1.903 6.405m-9.768-2.782L3.56 14.06a4.5 4.5 0 0 0 6.364 6.365l3.129-3.129m5.614-5.615 1.757-1.757a4.5 4.5 0 0 0-6.364-6.365l-4.5 4.5c-.258.26-.479.541-.661.84m1.903 6.405a4.495 4.495 0 0 1-1.242-.88 4.483 4.483 0 0 1-1.062-1.683m6.587 2.345 5.907 5.907m-5.907-5.907L8.898 8.898M2.991 2.99 8.898 8.9" />
+                            </svg> 
+                        </div>
                         Unlink Bank
                     </button>
                 </div>
@@ -259,9 +261,11 @@ const PlaidLinkComponent = ({ onSuccess }) => {
                             console.log("Plaid Link Exit:", error, metadata)
                         }
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6" width="20" height="20">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+                        <div style={{ display: 'inline-flex', marginRight: '5px', alignItems: 'center' }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 26 25" strokeWidth="1.3" stroke="currentColor" className="size-6" width="20" height="20">
+                                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
                             </svg>
+                        </div>
                         Link Bank
                     </PlaidLink>
                 </div>
